ea2f5086fee91cc600aec74982dadff08d103407
Mohammed-Kapiel <mohammed.kapiel@gmail.com>
2024-04-13 11:11:26 +0200

    Added Unit Movement and Path finding

---
+++ Managers/Units/Unit Stats/unit_stats.gd
@@ -0,0 +1,4 @@
+class_name Unit_Stats extends Resource
+
+@export var move_acceleration : float
+@export var move_speed : float
--- Objects/Units/unit.gd
+++ Objects/Units/unit.gd
@@ -1,8 +1,31 @@
 class_name Unit extends CharacterBody2D
 
+@export var stats : Unit_Stats
 @export var is_selected = false
 @onready var selection_overlay = $"Selection Overlay"
+@onready var navigation_agent = $NavigationAgent2D
+
+var is_following : bool = false
+@export var move_target : Vector2
+
 
 func select(is_true:bool = true):
 	is_selected = is_true
 	selection_overlay.visible = is_selected
+
+func _input(event):
+		if event.is_action("move"):
+			if is_selected:
+				move_target = get_global_mouse_position()
+				navigation_agent.target_position = move_target
+
+func _physics_process(delta):
+		var direction : Vector2 = navigation_agent.get_next_path_position() - global_position
+		direction = direction.normalized()
+		is_following = navigation_agent.is_target_reached()
+		if !is_following:
+			velocity = velocity.lerp(direction * stats.move_speed, stats.move_acceleration * delta)
+			move_and_slide()
+		
+
+	
